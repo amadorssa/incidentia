93982470986cd23e711cbefd624008fec3cb3f90
Julio Camargo <julio.camargo96@gmail.com>
2024-10-15 20:46:22 -0700

    La vista details maneja ya formato markdown

--- incidents/views.py
+++ incidents/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.views import generic
 from django.urls import reverse
-
+import markdown
 
 from .forms import IncidentForm
 from .models import Incident
@@ -28,7 +28,14 @@ class IndexView(generic.ListView):
 
 class DetailView(generic.DetailView):
     model = Incident
-    template_name = "incidents/detail.html"
+    template_name = 'incidents/detail.html'
+    context_object_name = 'incident'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Convertir la descripción de Markdown a HTML
+        context['incident'].description = markdown.markdown(context['incident'].description)
+        return context
 
 class EditIncidentView(generic.UpdateView):
     model = Incident
