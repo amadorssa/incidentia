ed0469d7b16b9f28692c1a4b4aaead6dd2a5abda
rulo89b <raul_tutu89@hotmail.com>
2024-10-24 11:15:46 -0700

    añadido fechas de inicio/vencimiento a tickets y tabla

--- incidents/forms.py
+++ incidents/forms.py
@@ -4,13 +4,18 @@ from .models import Incident
 class IncidentForm(forms.ModelForm):
     class Meta:
         model = Incident
-        fields = ['incident_text', 'user_creator', 'description']  
+        fields = ['incident_text', 'user_creator', 'description', 'start_date', 'due_date']  # Añadir los nuevos campos
         labels = {
             'incident_text': 'Nombre',
             'user_creator': 'Agregado por',
-            'description': 'Descripcion',
+            'description': 'Descripción',
+            'start_date': 'Fecha de inicio',
+            'due_date': 'Fecha de vencimiento',
         }
         widgets = {
             'description': forms.Textarea(attrs={'rows': 4, 'cols': 40}),
+            'start_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'due_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
         }
+
 
--- incidents/models.py
+++ incidents/models.py
@@ -5,13 +5,13 @@ from django.utils import timezone
 
 class Incident(models.Model):
     incident_text = models.CharField(max_length=200)
-    user_creator = models.CharField(max_length=16) #Cambiar por usuario con sesion iniciada
+    user_creator = models.CharField(max_length=16)  # Cambiar por usuario con sesión iniciada
     pub_date = models.DateTimeField('date published', auto_now_add=True)
     description = models.TextField(null=True, blank=True)  # Campo de descripción con soporte Markdown
     
-    #Regresar una descripcion del incidente
+    # Nuevos campos
+    start_date = models.DateTimeField('Fecha de inicio', null=True, blank=True)
+    due_date = models.DateTimeField('Fecha de vencimiento', null=True, blank=True)
+
     def __str__(self):
         return f"{self.incident_text} (by {self.user_creator} on {self.pub_date.strftime('%Y-%m-%d')})"
-
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
