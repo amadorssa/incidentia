7cf38cf554c6faadc209b80621609308b73ff088
rulo89b <raul_tutu89@hotmail.com>
2024-11-18 14:31:22 -0700

    historial para ticket implementado

--- incidents/models.py
+++ incidents/models.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from organizaciones.models import MiembroOrganizacion, Organizacion
+from cuentas.models import Usuario
+
 
 class Incident(models.Model):
     PRIORIDAD_CHOICES = [
@@ -44,6 +46,8 @@ class Incident(models.Model):
     last_modified = models.DateTimeField('Última modificación', auto_now=True)
     related_incidents = models.ManyToManyField('self', blank=True, symmetrical=False, related_name='related_to')
     estado = models.CharField(max_length=20, choices=ESTADO_CHOICES, default='nuevo')
+    usuario_modificador = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, related_name='modified_incidents')
 
 
     def clean(self):
@@ -52,6 +56,41 @@ class Incident(models.Model):
         
         if self.assigned_to and self.organizacion and self.assigned_to.organizacion != self.organizacion:
             raise ValidationError("El usuario asignado debe pertenecer a la organización del incidente.")
+        
+
+    def save(self, *args, **kwargs):
+        if self.pk:  # Solo registra cambios si el objeto ya existe
+            incidente_anterior = Incident.objects.get(pk=self.pk)
+            cambios = []
+
+            for campo in self._meta.fields:
+                campo_nombre = campo.name
+                valor_nuevo = getattr(self, campo_nombre)
+                valor_anterior = getattr(incidente_anterior, campo_nombre)
+
+                if valor_nuevo != valor_anterior:
+                    cambios.append(campo_nombre)
+
+            if cambios:
+                HistorialIncidente.objects.create(
+                    incidente=self,
+                    usuario=self.usuario_modificador,
+                    campos_modificados=cambios,
+                )
+
+        super().save(*args, **kwargs)
+
 
     def __str__(self):
         return f"{self.incident_text or 'Sin título'} (by {self.user_creator} on {self.pub_date.strftime('%Y-%m-%d')})"
+    
+class HistorialIncidente(models.Model):
+    incidente = models.ForeignKey(
+        Incident, on_delete=models.CASCADE, related_name='historial')
+    usuario = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    fecha = models.DateTimeField(auto_now_add=True)
+    campos_modificados = models.JSONField()  # Guarda los nombres de los campos modificados en formato JSON.
+
+    def __str__(self):
+        return f"Historial para incidente {self.incidente.id} por {self.usuario.nombre} {self.usuario.apellido if self.usuario else 'Desconocido'}"
